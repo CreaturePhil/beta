28b5b1d0db472bbe2c6df72dfa368e881cf37120
Phil <birkal@outlook.com>
2014-11-15 15:42:54 -0800

    Change render one line style

--- app/controllers/main_controller.js
+++ app/controllers/main_controller.js
@@ -1,15 +1,11 @@
 module.exports = {
 
   index: function(req, res) {
-    res.render('index', {
-      title: 'Beta'
-    });
+    res.render('index', { title: 'Beta' });
   },
 
   about: function(req, res) {
-    res.render('about', {
-      title: 'About'
-    });
+    res.render('about', { title: 'About' });
   }
 
 };
