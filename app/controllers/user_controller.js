d341bfcc361710b4c4810add42a29741a55d16de
Phil <birkal@outlook.com>
2014-11-15 22:22:59 -0800

    Fix lint errors

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -258,10 +258,12 @@ module.exports = {
       });
     },
     post: function(req, res, next) {
-      req.body.avatar && req.assert('avatar', 'Must be .png, .jpg, or .gif').regexMatch(/(https?:\/\/.*\.(?:png|jpg|gif))/i);
+      var VALID_IMG_REGEX = /(https?:\/\/.*\.(?:png|jpg|gif))/i;
+
+      if (req.body.avatar) req.assert('avatar', 'Must be .png, .jpg, or .gif').regexMatch(VALID_IMG_REGEX);
+      if (req.body.website) req.assert('website', 'Invalid website url.').regexMatch(/https?:\/\/.{1,}\..{1,}/);
       req.assert('username', 'Only letters and numbers are allow in username.').regexMatch(/^[A-Za-z0-9]*$/);
       req.assert('username', 'Username cannot be more than 30 characters.').len(1, 30);
-      req.body.website && req.assert('website', 'Invalid website url.').regexMatch(/https?:\/\/.{1,}\..{1,}/);
       req.assert('email', 'Email is not valid.').isEmail();
       req.assert('bio', 'Bio must be less than or equal to 160 characters.').len(0, 160);
 
