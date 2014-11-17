abd3defc4b06e68c6903bbf43b06016ac005c80f
Phil <birkal@outlook.com>
2014-11-16 17:00:54 -0800

    Update tests

--- test/app.js
+++ test/app.js
@@ -9,10 +9,50 @@ describe('GET /', function() {
   });
 });
 
+describe('GET /about', function() {
+  it('should return 200 OK', function(done) {
+    request(app)
+      .get('/about')
+      .expect(200, done);
+  });
+});
+
 describe('GET /404', function() {
   it('should return 404 Page Not Found', function(done) {
     request(app)
       .get('/404')
       .expect(404, done);
+  });
+});
+
+describe('GET /signup', function() {
+  it('should return 200 OK', function(done) {
+    request(app)
+      .get('/signup')
+      .expect(200, done);
+  });
+});
+
+describe('GET /login', function() {
+  it('should return 200 OK', function(done) {
+    request(app)
+      .get('/login')
+      .expect(200, done);
+  });
+});
+
+describe('GET /forgot_password', function() {
+  it('should return 200 OK', function(done) {
+    request(app)
+      .get('/forgot_password')
+      .expect(200, done);
+  });
+});
+
+describe('GET /settings/account', function() {
+  it('should return 302 Redirect', function(done) {
+    request(app)
+      .get('/forgot_password')
+      .expect(302, done);
   });
 });
