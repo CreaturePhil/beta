266d68b7bbec67cfdf9bf784e9fd33236605a7d6
Phil <birkal@outlook.com>
2014-11-16 16:04:43 -0800

    Add models user test

---
+++ test/models.js
@@ -0,0 +1,46 @@
+var chai = require('chai');
+var should = chai.should();
+var User = require('../app/models/user');
+
+describe('User Model', function() {
+  it('should create a new user', function(done) {
+    var user = new User({
+      uid: 'test',
+      username: 'Test',
+      email: 'test@gmail.com',
+      password: 'password'
+    });
+    user.save(function(err) {
+      if (err) return done(err);
+      done();
+    });
+  });
+
+  it('should not create a user with the unique email', function(done) {
+    var user = new User({
+      uid: 'test1',
+      username: 'Test1',
+      email: 'test@gmail.com',
+      password: 'password'
+    });
+    user.save(function(err) {
+      if (err) err.code.should.equal(11000);
+      done();
+    });
+  });
+
+  it('should find user by email', function(done) {
+    User.findOne({ email: 'test@gmail.com' }, function(err, user) {
+      if (err) return done(err);
+      user.email.should.equal('test@gmail.com');
+      done();
+    });
+  });
+
+  it('should delete a user', function(done) {
+    User.remove({ email: 'test@gmail.com' }, function(err) {
+      if (err) return done(err);
+      done();
+    });
+  });
+});
